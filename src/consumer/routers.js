4d9b409d5dc57c232ee071ec3d08e4a907a66509
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2019-10-17 16:45:02 +0700

    'update'

--- src/consumer/routers.js
+++ src/consumer/routers.js
@@ -1,4 +1,13 @@
 const Router = require('koa-router')
+const { Consumer, KafkaClient } = require('kafka-node')
+
+const { TOPIC, KAFKA_HOST } = process.env
+const client = new KafkaClient({ kafkaHost: KAFKA_HOST })
+const consumer = new Consumer(client, [{ topic: TOPIC }])
+
+consumer.on('message', function(message) {
+  console.log(message)
+})
 
 const r = new Router()
 
--- src/producer/routers.js
+++ src/producer/routers.js
@@ -1,9 +1,34 @@
 const Router = require('koa-router')
+const { Producer, KafkaClient } = require('kafka-node')
+
+const { TOPIC, KAFKA_HOST } = process.env
+const client = new KafkaClient({ kafkaHost: KAFKA_HOST })
+const producer = new Producer(client)
+
+producer.on('ready', function() {
+  console.log('PRODUCER ready')
+})
+
+const send = messages =>
+  new Promise((resolve, reject) => {
+    producer.send([{ topic: TOPIC, messages }], function(err, result) {
+      if (err) {
+        return reject(err)
+      }
+      return resolve(result)
+    })
+  })
 
 const r = new Router()
 
 r.get('/producer', async ctx => {
   await ctx.render('producer', { page: 'Producer' })
+})
+
+r.post('/producer', async ctx => {
+  const { body } = ctx.request
+  const result = await send(body)
+  ctx.body = result
 })
 
 module.exports = r
